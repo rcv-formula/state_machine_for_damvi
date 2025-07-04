8be89be33c5053fff661faaebae45f9c9403d900
KIMJONGHAK <86517369+JHlab123@users.noreply.github.com>
2025-07-04 21:11:38 +0900

    Delete object_detector/build directory
    mistake

--- object_detector/build/object_detection/CMakeFiles/delay_monitor_node.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for delay_monitor_node.
-# This may be replaced when dependencies are built.
--- object_detector/build/object_detection/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rcv/object_detection/object_detector
-# Build directory: /home/rcv/object_detection/object_detector/build/object_detection
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
